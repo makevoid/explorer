4b5c251f749ccfa50bada8281a0a2a90fd9b1781
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-02-16 21:01:39 +0000

    local all lifelong

--- config/env.rb
+++ config/env.rb
@@ -6,21 +6,24 @@ APP_ENV = ENV["RACK_ENV"] || "development"
 path = File.expand_path "../../", __FILE__
 APP_PATH = path
 
-RPC_HOST = '188.165.223.5' # sys.mkvd.net
+# RPC_HOST = '188.165.223.5' # sys.mkvd.net
 # RPC_HOST = '212.47.233.106' # bchain  # scaleway
-# RPC_HOST = 'localhost'
+RPC_HOST = 'localhost' # always localhost, localhost ftw!
 
 
 read = -> (path) { File.read File.expand_path path }
 
-password = if APP_ENV == "development"
-  path = "~/.sys.bitcoin.conf.pw"
-  read.( path )
-else
+# password = if APP_ENV == "development"
+#   path = "~/.sys.bitcoin.conf.pw"
+#   read.( path )
+# else
+
+  # local is better:
+
   path = "~/.bitcoin/bitcoin.conf"
   file = read.( path )
   file.strip.match(/rpcpassword=(.+)/)[1]
-end
+# end
 
 RPC_PASSWORD = password.strip
 
